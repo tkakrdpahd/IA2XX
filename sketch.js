ccfd3b7f26248d7a3c491d1b1e78873d748196ec
tkakrdpahd <89321156+tkakrdpahd@users.noreply.github.com>
2024-04-19 11:05:53 -0400

    tested
    tested

--- sketch.js
+++ sketch.js
@@ -7,42 +7,37 @@ function preload() {
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    // this is raytracing setting
+    wall = new Boundary(300, 100, 300, 300);
+    ray = new Ray(100, 200);
 }
 
 function draw() {
     background(0);
 
-    // thhis is raytracing setting
-    wall = new Boundary(300, 100, 300, 300);
-    ray = new Ray(100, 200);
-
     wall.show();
     ray.show();
+    ray.lookAt(mouseX, mouseY);
 
     let pt = ray.cast(wall);
     console.log(pt);
-    // if (pt) {
-    //     fill(255);
-    //     ellipse(pt.x, pt.y, 8, 8);
-    // }
+    if (pt) {
+         fill(255);
+         ellipse(pt.x, pt.y, 8, 8);
+    }
 
     /*
-
     // This is call png
 
     let targetWidth = windowWidth * 0.75;
     let targetHeight = windowHeight * 0.75;
-
     let imgAspectRatio = img.width / img.height;
-
     let drawWidth, drawHeight;
 
     if (targetWidth / targetHeight > imgAspectRatio) {
-        // 타겟 크기가 이미지보다 가로로 넓은 경우: 이미지의 높이를 타겟 높이에 맞추고, 가로 길이를 비율에 맞게 조정
         drawHeight = targetHeight;
         drawWidth = img.width * (targetHeight / img.height);
     } else {
-        // 타겟 크기가 이미지보다 세로로 긴 경우: 이미지의 너비를 타겟 너비에 맞추고, 세로 길이를 비율에 맞게 조정
         drawWidth = targetWidth;
         drawHeight = img.height * (targetWidth / img.width);
     }
@@ -53,14 +48,13 @@ function draw() {
     image(img, startX, startY, drawWidth, drawHeight);
 
     mouseLocation();
-
     */
 }
 
-function mouseLocation() {
-    let normalizedX = mouseX / windowWidth;
-    let normalizedY = mouseY / windowHeight;
-    if (mouseIsPressed === true) {
-        console.log(normalizedX, normalizedY);
-    }
-}
+// function mouseLocation() {
+//     let normalizedX = mouseX / windowWidth;
+//     let normalizedY = mouseY / windowHeight;
+//     if (mouseIsPressed === true) {
+//         console.log(normalizedX, normalizedY);
+//     }
+// }
