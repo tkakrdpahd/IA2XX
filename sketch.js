eef6038ec5479efe669169009aa39e5e47314ce2
tkakrdpahd <89321156+tkakrdpahd@users.noreply.github.com>
2024-04-22 12:57:31 -0400

    tested
    tested

--- sketch.js
+++ sketch.js
@@ -1,77 +1,74 @@
 let img;
+let json;
 let walls = [];
-let ray, particle;
+let ray;
+let particle;
 let xoff = 0;
 let yoff = 10000;
+let vwr, vhr;
 
-function preload() {
+async function preload() {
     img = loadImage('img/Home.png');
+
+    // setWindowRatio
+    if (windowWidth > windowHeight) {
+        vwr = (4*windowHeight)/3;
+        vhr = windowHeight;
+    } else {
+        vwr = windowWidth;
+        vhr = (3*windowWidth)/4;
+    }
+
+    json = await getJson();
+    console.log(json);
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    // this is raytracing setting
-    for (let i = 0; i < 5; i++) {
-        let x1 = random(width);
-        let x2 = random(width);
-        let y1 = random(height);
-        let y2 = random(height);
-        walls[i] = new Boundary(x1, y1, x2, y2);
+    createCanvas(vwr, vhr);
+    // this is boundary
+    walls.push(new Boundary(-1, -1, width, -1));
+    walls.push(new Boundary(width, -1, width, height));
+    walls.push(new Boundary(width, height, -1, height));
+    walls.push(new Boundary(-1, height, -1, -1));
+
+    for (let i = 0; i < json.length; i += 1) {
+        walls.push(new Boundary(json[i][0]*vwr, json[i][1]*height, json[i][2]*vwr, json[i][3]*height));
+        walls.push(new Boundary(json[i][2]*vwr, json[i][3]*height, json[i][4]*vwr, json[i][5]*height));
+        walls.push(new Boundary(json[i][4]*vwr, json[i][5]*height, json[i][6]*vwr, json[i][7]*height));
+        walls.push(new Boundary(json[i][6]*vwr, json[i][7]*height, json[i][0]*vwr, json[i][1]*height));
     }
-    particle = new Particle();
+
+    particle = new Particle(); // make particleN
 }
 
 function draw() {
     background(0);
     for (let wall of walls) {
-        wall.show();
+    wall.show();
     }
-    particle.update(noise(xoff) * width, noise(yoff) * height);
+    particle.update(mouseX, mouseY);
     particle.show();
     particle.look(walls);
 
     xoff += 0.01;
     yoff += 0.01;
 
-    // ray.show();
-    // ray.lookAt(mouseX, mouseY);
-
-    // let pt = ray.cast(wall);
-    // console.log(pt);
-    // if (pt) {
-    //      fill(255);
-    //      ellipse(pt.x, pt.y, 8, 8);
-    // }
-
-    /*
     // This is call png
+    image(img, 0, 0, vwr, vhr);
 
-    let targetWidth = windowWidth * 0.75;
-    let targetHeight = windowHeight * 0.75;
-    let imgAspectRatio = img.width / img.height;
-    let drawWidth, drawHeight;
-
-    if (targetWidth / targetHeight > imgAspectRatio) {
-        drawHeight = targetHeight;
-        drawWidth = img.width * (targetHeight / img.height);
-    } else {
-        drawWidth = targetWidth;
-        drawHeight = img.height * (targetWidth / img.width);
-    }
-
-    let startX = (windowWidth - drawWidth) / 2;
-    let startY = (windowHeight - drawHeight) / 2;
-
-    image(img, startX, startY, drawWidth, drawHeight);
-
-    mouseLocation();
-    */
+    console.log(mouseLocation());
 }
 
 function mouseLocation() {
-    let normalizedX = mouseX / windowWidth;
-    let normalizedY = mouseY / windowHeight;
+    let normalizedX = mouseX / vwr;
+    let normalizedY = mouseY / vhr;
     if (mouseIsPressed === true) {
-        console.log(normalizedX, normalizedY);
+        return [normalizedX, normalizedY];
     }
+}
+
+async function getJson() {
+    const response = await fetch("walls.json");
+    const data = await response.json();
+    return data;
 }
